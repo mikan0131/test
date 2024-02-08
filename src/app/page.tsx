f340133706effac8869109a8759c8a9d642fd313
mikan0131 <148690715+mikan0131@users.noreply.github.com>
2024-02-08 17:22:47 +0900

    add file

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1> Hello World! </h1>
+      <ul>
+        <p> apple </p>
+        <p> orange </p>
+        <p> banana </p>
+      </ul>
+    </main>
+  );
+}
---
+++ src/app/path1/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1> Hello World! </h1>
+      <a href = "https://prog-8.com"> Progate </a>
+    </main>
+  );
+}
